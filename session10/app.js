a92fecc3ea0302563bac70af780720d3ea4fa85c
vhaovvvx <vhaovvvx@gmail.com>
2021-04-29 07:06:19 +0700

    asdasd

--- session10/app.js
+++ session10/app.js
@@ -1,4 +1,7 @@
-const todoApi = "https://jsonplaceholder.typicode.com/todos";
+const renderElement = document.getElementById("myUL");
+let completed = undefined;
+const todoApi = "http://localhost:3000/todo";
+const createBtn = document.querySelector(".addBtn");
 
 function todoList() {
   return fetch(todoApi).then((res) => res.json());
@@ -13,30 +16,51 @@ start();
 
 let todoLists = "";
 let i = 0;
-let selectedId = undefined;
 
 function clickHandler() {
-  todoList().then((todolists) => {
-    todolists.forEach((todo) => {
-      todoLists = `
-                <li class="todo-items" id=todo-items-${todo.id} data-id=${todo.id}>
-                ${todo.userId} ${todo.id} ${todo.title} <b>${todo.completed}</b>    <span class="close" onclick=handleDeleteItem(${todo.id})>X</span>
-                </li>
-                `;
-      document.getElementById("myUL").innerHTML += todoLists;
-    });
-
-    const liElement = document.querySelectorAll("li");
-    liElement.forEach((lielm) => {
-      lielm.addEventListener("click", function (e) {
-        e.target.classList.toggle("checked");
-        // console.log(selectedId);
-        // const selectedUser = todolists.find((todo) => todo.id == selectedId);
-        // console.log(selectedUser);
+  todoList()
+    .then((todolists) => {
+      todolists.forEach((todo) => {
+        todoLists = `
+                  <li class="todo-items" id=todo-items-${todo.id} value=${todo.id}>
+                    ${todo.userId} ${todo.id} ${todo.title} <b>${todo.completed}</b>
+                    <span class="edit" id="edit" onclick=handleEditItem(${todo.id})>Edit</span>
+                    <span class="close" onclick=handleDeleteItem(${todo.id})>X</span>
+                  </li>
+                  `;
+        renderElement.innerHTML += todoLists;
       });
-    });
-  });
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally();
 }
+
+renderElement.addEventListener("click", function (e) {
+  const id = e.target.value;
+  e.preventDefault();
+  var checked = e.target.classList.toggle("checked");
+  if (checked == true) {
+    completed = false;
+  } else if (checked == false) {
+    completed = true;
+  }
+  let optionss = {
+    method: "PATCH",
+    body: JSON.stringify({
+      completed: completed,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+  fetch(todoApi + "/" + id, optionss)
+    .then((res) => res.json())
+    .then((a) => {
+      // location.reload();
+    });
+});
 
 function handleDeleteItem(id) {
   var options = {
@@ -47,7 +71,7 @@ function handleDeleteItem(id) {
   };
   fetch(todoApi + "/" + id, options)
     .then((res) => res.json())
-    .then((todo) => {
+    .then(() => {
       let deletee = document.querySelector("#todo-items-" + id);
       deletee.remove();
     });
@@ -64,23 +88,43 @@ function creatElement(data) {
   fetch(todoApi, optionss)
     .then((res) => res.json())
     .then((elementsAdd) => {
-      let liHtmls = "";
-      liHtmls += `<li class="todo-items">
-      ${elementsAdd.name} ${elementsAdd.id}
-      </li>`;
-      document.getElementById("myUL").innerHTML += liHtmls;
+      const dataArrNew = [];
+      dataArrNew.push(elementsAdd);
+      location.reload();
+      // clickHandler(todolists);
     });
 }
 
 function handleCreate() {
-  let createBtn = document.querySelector(".addBtn");
   createBtn.onclick = function () {
     let name = document.querySelector('input[name="name"]').value;
-    var data = {
-      name: name,
-    };
 
+    var data = {
+      title: name,
+      userId: "",
+      id: "",
+      completed: "",
+    };
+    let changDefault = document.querySelector("#myInput");
+    console.log(changDefault);
+    changDefault.value = "";
     creatElement(data);
   };
 }
 handleCreate();
+
+function handleEditItem(id) {
+  let editButtons = prompt("Enter content Edit");
+  let optionss = {
+    method: "PATCH",
+    body: JSON.stringify({
+      title: editButtons,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+  fetch(todoApi + "/" + id, optionss)
+    .then((res) => res.json())
+    .then(() => location.reload());
+}
